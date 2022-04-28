4fde3cfd24fbe8de839605358829e3c1f8d2835c
Marcelo <3338@holbertonschool.com>
2022-04-28 17:34:39 -0300

    D

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,8 +1,9 @@
 -- Write a SQL script that creates a stored procedure ComputeAverageScoreForUser
 -- that computes and store the average score for a student.
 DELIMITER $$
+DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser$$
 CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
 BEGIN
-    SELECT AVG(score) INTO @average_score FROM corrections WHERE user_id=user_id;
-    UPDATE users SET average_score = @average_score WHERE id=user_id;
+    SELECT AVG(score) INTO @average_score FROM corrections WHERE corrections.user_id = user_id;
+    UPDATE users SET average_score = @average_score WHERE id = user_id;
 END$$
