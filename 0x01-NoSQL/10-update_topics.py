f0fcc7cd0776fc5955d03b3cbb07bdfce894bcb9
marceloarbiza <3338@holbertonschool.com>
2022-05-03 16:16:40 -0700

    D

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Write a Python function that changes all
+topics of a school document based on the name
+"""
+
+
+import pymongo
+
+
+def update_topics(mongo_collection, name, topics):
+    """ a comment """
+    mongo_collection.find_one_and_update({'name':name},{'$set':{'topics':topics}})
