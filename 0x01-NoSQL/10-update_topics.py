0d9cc390ef425a61a7514c534cff7f50cfa06c28
marceloarbiza <3338@holbertonschool.com>
2022-05-03 16:20:29 -0700

    D

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -10,4 +10,5 @@ import pymongo
 
 def update_topics(mongo_collection, name, topics):
     """ a comment """
-    mongo_collection.find_one_and_update({'name':name},{'$set':{'topics':topics}})
+    mongo_collection.find_one_and_update({'name': name},
+                                         {'$set': {'topics': topics}})
