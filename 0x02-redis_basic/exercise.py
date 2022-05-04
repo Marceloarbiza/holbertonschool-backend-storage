9f94b2c157a11fd2920b80021c5323fbc9c007db
Marcelo <3338@holbertonschool.com>
2022-05-04 19:53:09 -0300

    D

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -9,10 +9,12 @@ import uuid
 class Cache:
 
     def __init__(self):
+        """ init redis """
         self._redis = redis.Redis()
         self._redis.flushdb()
 
     def store(self, data):
+        """ store redis """
         key = uuid.uuid4()
         self._redis.set(str(key), data)
         return str(key)
