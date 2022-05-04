7f5e06de67fb286a9e281d39210d0d2a27c0b15a
marceloarbiza <3338@holbertonschool.com>
2022-05-03 18:49:02 -0700

    D

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -9,15 +9,10 @@ import pymongo
 
 def top_students(mongo_collection):
     """ a comment """
-    li = []
-    x = mongo_collection.find({}, {'_id': 1, 'name': 1, 'topics': 1})
-    for i in x:
-        lito = []
-        lii = i.get('topics')
-        for h in lii:
-            lito.append(h.get('score'))
-        suma = sum(lito)
-        leni = len(lito)
-        i['averageScore'] = suma/leni
-        li.append(i)
-    return li
+    return mongo_collection.aggregate([
+        {'$project': {
+            'name': 1,
+            'averageScore': {'$avg': '$topics.score'}
+        }},
+        {'$sort': {'averageScore': -1}}
+    ])
